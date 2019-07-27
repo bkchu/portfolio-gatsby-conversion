b0a4775cd5c00053e4eba8968db2984451f34745
Brandon Chung <brandon.k.chung@gmail.com>
2019-07-27 01:12:15 -0500

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,14 @@
 import React from "react"
-
 import SEO from "../components/seo"
+import { Link } from 'gatsby';
 
 const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
+  <div className="FourOhFour">
+    <SEO title="404" />
+    <h1 className="FourOhFour__header">404</h1>
+    <p className="FourOhFour__subheader">You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link className="FourOhFour__link" to='/'>Back Home</Link>
+  </div>
 )
 
 export default NotFoundPage
