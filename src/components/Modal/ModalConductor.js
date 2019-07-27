7eab7027e50401ea038e2f487ebff47f3b0a7059
Brandon Chung <brandon.k.chung@gmail.com>
2019-07-27 01:20:51 -0500

    remove react google analytics

--- src/components/Modal/ModalConductor.js
+++ src/components/Modal/ModalConductor.js
@@ -3,14 +3,12 @@ import { connect } from 'react-redux';
 import * as actions from '../../redux/ducks/reducer';
 import ProjectModal from './ModalContent/ProjectModal';
 import ContactModal from './ModalContent/ContactModal';
-import ReactGA from 'react-ga';
 
 const ModalConductor = props => {
   switch (props.currentModal.modalType) {
     case 'PROJECT_MODAL':
       return <ProjectModal {...props} />;
     case 'CONTACT_MODAL':
-      ReactGA.modalview('/contact');
       return <ContactModal {...props} />;
     // case "HIDE_MODAL":
     //   return <div />;
