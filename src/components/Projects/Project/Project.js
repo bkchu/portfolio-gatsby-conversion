ea797bd48b335c01a3f57e565d77614854f4aafd
Brandon Chung <brandon.k.chung@gmail.com>
2019-08-16 21:50:58 -0500

    added project animations

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -1,16 +1,30 @@
 import React from "react"
 import { connect } from "react-redux"
 import { projectModal } from "../../../redux/ducks/reducer"
+import { motion } from "framer-motion"
 
 const Project = props => {
-  const {
-    title,
-    img,
-    color,
-  } = props.project
+  const { title, img, color } = props.project
+
+  const projectVariants = {
+    opened: {
+      y: 0,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        y: { type: "spring", stiffness: 50 },
+        scale: { type: "spring", stiffness: 50 },
+      },
+    },
+    closed: {
+      y: 20,
+      opacity: 0,
+      scale: 0.95,
+    },
+  }
 
   return (
-    <div key={title} className="Project">
+    <motion.div variants={projectVariants} key={title} className="Project">
       <div
         onClick={() => props.projectModal(props.project)}
         style={{ background: color }}
@@ -19,7 +33,7 @@ const Project = props => {
         <img className="Project__img" src={img} alt={title} />
         <h1 className="Project__title">{title}</h1>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,34 @@
 import React from "react"
+import { motion } from "framer-motion"
 import Page from "../Page/Page"
 import Project from "./Project/Project"
 
 import projects from "./project_util"
 
 const Projects = () => {
-  const projectDisplay = projects.map(project => (
+  const projectsVariants = {
+    opened: {
+      transition: { staggerChildren: 0.07, delayChildren: 1 },
+    },
+    closed: {
+      transition: { staggerChildren: 0.05, staggerDirection: -1 },
+    },
+  }
+
+  const projectDisplay = projects.map((project, i) => (
     <Project key={project.title} project={project} />
   ))
+
   return (
     <Page title="Projects">
-      <div className="Projects">{projectDisplay}</div>
+      <motion.div
+        initial="closed"
+        animate="opened"
+        variants={projectsVariants}
+        className="Projects"
+      >
+        {projectDisplay}
+      </motion.div>
     </Page>
   )
 }
